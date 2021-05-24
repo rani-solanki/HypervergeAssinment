d44b30ada994a14baa8a626880553328f801ecd4
rani-solanki <rani19@navgurukul.org>
2021-05-24 16:45:05 +0530

    status

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 const fs = require('fs')
 const readFile = require("readline-sync")
+var utils = require('utils').string;
 
-
-cammnd = [ ]
+Allparking = []
 array = []
-fs.readFile('input.txt', 'utf-8', (error, data) => {
+const Alldata = fs.readFile('input.txt', 'utf-8', (error, data) => {
     if (error) throw error;
+    // create all slots 
     var array = data.toString().split("\n");
-    // console.log(data.toString())
-    console.log(array)
+    cammnds(array)
 })
 
-
 function cammnds(input) {
-    var element = input.split(" ")[0];
-    switch (element) {
-        case create_parking_lot:
-        var parkings = utils.create_parking_lot[input]
-            for (var i = 0; i < parkings.Elength; i++){
-                var arr = new Object();
-                arr[costumer(i)] = null;
-                parkings.push(obj);
-            }
+    // Allocated slot number: 1
+    obj = {}
+    Allparking = []
+    for (var i = 0; i < input.length; i++){
+        obj[i + 1] = array[i]
+        Allparking.push(obj)
+    }
+
+    console.log(Allparking)
+}
+
+
+for (var i = 0; i < array.length; i++) {
+    var element = array.split(" ")[0];
+    console.log(element)
+
+    if (element == "park") {
+        if (element in array) {
+            console.log("this slots is free", i)
+        } else {
+            console.log("this slot is not free")
+        }
+
+        console.log(Allparking)
+
+    } else if (element == "leave") {
+        array[i].remove(function (e) {
+            return e === "leave";
+        });
+        
+        console.log(array);
+    } else {
+        console.log(array[i])
     }
 }
