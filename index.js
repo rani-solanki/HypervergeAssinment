7ff7a4a736b9a696b44f9ddde6ad6c817b238f5f
rani-solanki <rani19@navgurukul.org>
2021-05-25 00:19:58 +0530

    done

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const readFile = require("readline-sync")
 
 Allparking = []
 array = []
-
-const Alldata = fs.readFile('input.txt', 'utf-8', (error, data) => {
+fs.readFile('input.txt', 'utf-8', (error, data) => {
     if (error) throw error;
     var array = data.toString().split("\n");
     for (var i = 0; i < array.length; i++) {
@@ -12,16 +11,31 @@ const Alldata = fs.readFile('input.txt', 'utf-8', (error, data) => {
     }
 })
 
+function status(array) {
+    alocated_arr = []
+    for (var i = 1; i < 6; i++) {
+        let sta = array[i].split(" ")[0];
+        if (!( sta == status && leave)) {
+            let carNumber = array[i].split(" ")[1];
+            alocated_arr.push(carNumber)
+        }
+        else {
+            break
+        }
+    }
+    console.log(alocated_arr)
+}
+
+
 function commands(input, array) {
-    var element = input.split(" ")[0];
-    
+    let element = input.split(" ")[0];    
     switch (element) {
         case "create_parking_lot":
+            console.log('created parking slots 6')
             for (var i = 0; i < 6; i++) {
                 console.log("Allocated slot number", ':', i + 1)
             }
             break;
-        
         case "park":
             console.log("Sorry, parking lot is full", 1)
             break;
@@ -30,12 +44,13 @@ function commands(input, array) {
             var carNumber = input.split(" ")[1];
             console.log("Registration number" + carNumber + "with Slot Number" + 1 + "is free with Charge" + 30)
             break;
+        
         case "status":
-
-            
+            status(array)
+            break;
     }       
-
 }
+
 
 
 
